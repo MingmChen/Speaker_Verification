84164f0c17689f8f1e5d1cf3a406af1245f3ed70
Polydefkis <polydefkis.d.gkagkos@gmail.com>
2019-05-27 18:51:34 +0200

    fixed gcloud wrapers

--- gcloud_wrappers.py
+++ gcloud_wrappers.py
@@ -1,45 +1,18 @@
-import subprocess
 import time
 from googleapiclient import discovery
 from oauth2client.client import GoogleCredentials
-# from google.cloud import storage
-
-args = ['nohup', 'python', '-u', '/Users/polaras/PycharmProjects/Speech_recognition_Project/train.py',
-        '>', 'log.txt', '&']
-
-start_vm_speech = ['/Users/polaras/Documents/google-cloud-sdk/bin/gcloud', 'compute', 'instances', 'start',
-                   'dt2119-speaker-verification-vm']
-
-stop_vm_speech = ['gcloud', 'compute', 'instances', 'stop', 'dt2119-speaker-verification-vm']
-
-copy_file_speech = ['/Users/polaras/Documents/google-cloud-sdk/bin/gcloud', 'compute', 'scp', '--project',
-                    'dt2119-speaker-verification-vm',
-                    '--zone', 'europe-west1-b', '--recurse',
-                    '/Users/polaras/Downloads/termpapersVT2017', 'polaras@test-instance:/home/polaras/']
-
-start_vm_test = ['/Users/polaras/Documents/google-cloud-sdk/bin/gcloud', 'compute', 'instances', 'start',
-                 'test-instance']
-stop_vm_test = ['/Users/polaras/Documents/google-cloud-sdk/bin/gcloud', 'compute', 'instances', 'stop', 'test-instance']
-
-copy_file_test = ['/Users/polaras/Documents/google-cloud-sdk/bin/gcloud', 'compute', 'scp', '--project',
-                  'test-instance',
-                  '--zone', 'us-east1-b', '--recurse',
-                  '/Users/polaras/Downloads/termpapersVT2017', 'polaras@test-instance:/home/polaras/']
 
 
-def main():
-    subprocess.Popen(start_vm_test)
-    time.sleep(100)
-    subprocess.Popen(copy_file_test)
-    subprocess.Popen(stop_vm_test)
+def start_speech_vm(compute=None):
+    if compute is None:
+        credentials = GoogleCredentials.get_application_default()
 
+        compute = discovery.build(
+            'compute',
+            'v1',
+            credentials=credentials
+        )
 
-# subprocess.Popen(start_vm_speech)
-# subprocess.Popen(copy_file_speech)
-# subprocess.Popen(stop_vm_speech)
-
-
-def start_speech_vm(compute):
     start_request = compute.instances().start(
         project='dt2119-speaker-verification',
         zone='europe-west1-b',
@@ -55,7 +28,16 @@ def start_speech_vm(compute):
     )
 
 
-def stop_speech_vm(compute):
+def stop_speech_vm(compute=None):
+    if compute is None:
+        credentials = GoogleCredentials.get_application_default()
+
+        compute = discovery.build(
+            'compute',
+            'v1',
+            credentials=credentials
+        )
+
     stop_request = compute.instances().stop(
         project='dt2119-speaker-verification',
         zone='europe-west1-b',
@@ -88,7 +70,7 @@ def wait_for_operation(compute, project, zone, operation):
 
         time.sleep(1)
 
-
+# from google.cloud import storage
 # def upload_blob(bucket_name, source_file_name, destination_blob_name):
 #     """Uploads a file to the bucket."""
 #     storage_client = storage.Client()
@@ -101,13 +83,8 @@ def wait_for_operation(compute, project, zone, operation):
 
 
 if __name__ == "__main__":
-    credentials = GoogleCredentials.get_application_default()
 
-    compute = discovery.build(
-        'compute',
-        'v1',
-        credentials=credentials
-    )
 
-    start_speech_vm(compute)
+    # start_speech_vm()
     # stop_speech_vm(compute)
+    pass
