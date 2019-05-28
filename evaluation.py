bdddbb4f7719744f9afa39218e43d56d48100d77
Polydefkis <polydefkis.d.gkagkos@gmail.com>
2019-05-28 20:23:14 +0200

    Add EER and Roc Plot with K fold cross validation

--- evaluation.py
+++ evaluation.py
@@ -1,11 +1,51 @@
-import numpy as np
-import torch.nn.init as init
 from sklearn.metrics.pairwise import cosine_similarity
-import torchvision.transforms as transforms
 from utils import *
 from model import *
 import os
 import matplotlib.pyplot as plt
+
+
+def get_and_plot_k_eer_auc(label, scores, k=1):
+    step = int(label.shape[0] / float(k))
+    EER_VECTOR = np.zeros((k, 1))
+    AUC_VECTOR = np.zeros((k, 1))
+
+    fig = plt.figure()
+    ax = fig.gca()
+
+    for split_num in range(k):
+        index_start = split_num * step
+        index_end = (split_num + 1) * step
+
+        EER_temp, AUC_temp, fpr, tpr = get_eer_auc(label[index_start:index_end], scores[index_start:index_end])
+
+        EER_VECTOR[split_num] = EER_temp
+        AUC_VECTOR[split_num] = AUC_temp
+
+        plt.setp(plt.plot(fpr, tpr, label='{} split'.format(split_num)), linewidth=2)
+
+    print("EER=", np.mean(EER_VECTOR) * 100)
+    print("AUC=", np.mean(AUC_VECTOR) * 100)
+
+    ax.set_xticks(np.arange(0, 1.1, 0.1))
+    ax.set_yticks(np.arange(0, 1.1, 0.1))
+
+    plt.title('ROC with {}-fold cross validation'.format(k))
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.grid()
+    plt.show()
+
+
+def get_eer_auc(label, distance):
+    fpr, tpr, thresholds = roc_curve(label, distance, pos_label=1)
+    auc = roc_auc_score(label, distance)
+    eer = brentq(lambda x: 1. - x - interp1d(fpr, tpr)(x), 0., 1.)
+    eer2 = fpr[np.abs(fpr - (1 - tpr)).argmin(0)]
+
+    if np.array_equal(eer, eer2):
+        print("SAME")
+    return eer, auc, fpr, tpr
 
 
 class Evaluation:
@@ -36,8 +76,16 @@ class Evaluation:
 
 
 if __name__ == '__main__':
-    model = C3D2(n_labels=100, num_channels=1)
-    x = torch.rand((1, 1, 20, 80, 40))
-    dir_path = "C:/Users/anala/Desktop/coursecontent/Speech_and_Speaker/Project/speaker_models"
-    eval = Evaluation(model, x, dir_path)
-    eval.compute_Similarity()
+    # model = C3D2(n_labels=100, num_channels=1)
+    #
+    # x = torch.rand((1, 1, 20, 80, 40))
+    #
+    # dir_path = "C:/Users/anala/Desktop/coursecontent/Speech_and_Speaker/Project/speaker_models"
+    # eval = Evaluation(model, x, dir_path)
+    # eval.compute_Similarity()
+    labels = np.array([[1., 0.], [1., 0.]])
+    scores = np.array([[0.7, 0.3], [0.2, 0.8]])
+    print(labels.flatten())
+    # fpr, tpr, thresholds = roc_curve(labels[0], scores[0], pos_label=1)
+    get_and_plot_k_eer_auc(labels.flatten(), scores.flatten(), k=1)
+    
